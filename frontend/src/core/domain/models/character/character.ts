106a5cf26373a98daa4135a375838c47f8b8c23c
Harold Cohen <h.cohen.apple@icloud.com>
2021-10-10 23:33:42 +0200

    Update

--- frontend/src/core/domain/models/character/character.ts
+++ frontend/src/core/domain/models/character/character.ts
@@ -1,3 +1,29 @@
 export default class Character {
+    get level(): number {
+        return 1;
+    }
 
+    get rank(): number {
+        return 1;
+    }
+
+    get skillPoints(): number {
+        return 12;
+    }
+
+    get healthPoints(): number {
+        return 10;
+    }
+
+    get attackPoints(): number {
+        return 0;
+    }
+
+    get defensePoints(): number {
+        return 0;
+    }
+
+    get magikPoints(): number {
+        return 0;
+    }
 }
--- frontend/tests/character/unit/ICreateACharacter.test.ts
+++ frontend/tests/character/unit/ICreateACharacter.test.ts
@@ -14,5 +14,12 @@ describe('As a player, I can create a character', () => {
     it('That starts at level 1, rank 1 with 12 SP, 10 HP, 0 AP, 0 DP, 0 MP.', async () => {
         const createdCharacter = await new ICreateACharacter().execute();
         expect(createdCharacter).toBeInstanceOf(Character);
+        expect(createdCharacter.level).toEqual(1);
+        expect(createdCharacter.rank).toEqual(1);
+        expect(createdCharacter.skillPoints).toEqual(12);
+        expect(createdCharacter.healthPoints).toEqual(10);
+        expect(createdCharacter.attackPoints).toEqual(0);
+        expect(createdCharacter.defensePoints).toEqual(0);
+        expect(createdCharacter.magikPoints).toEqual(0);
     });
 });
